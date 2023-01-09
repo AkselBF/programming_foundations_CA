48d0eecf89e224af227a9484ff9d11718fa3364a
AkselBF <aksel.boutrouefaret@yahoo.com>
2023-01-09 16:45:58 +0100

    start

---
+++ choice.js
@@ -0,0 +1,11 @@
+/*
+  list accessed from array
+*/
+
+const names = ["first", "second", "third", "fourth"]; // Array
+let type = "someone";  // String
+let average_age = 20; // Number
+let test = true; // Boolean
+
+//function
+function listing(names, type, test) {}
