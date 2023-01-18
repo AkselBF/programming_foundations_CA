78ced8207875ea643e37594f12d4834a1f27cce3
AkselBF <aksel.boutrouefaret@yahoo.com>
2023-01-18 15:54:19 +0100

    Update car.js

--- car.js
+++ car.js
@@ -3,19 +3,21 @@
 */
 
 const car = {
+
+  // Nested object
   brand: {
     name: "Opel",
     from: "Norway"
   },
-  running: true,
-  gear: ["Neutral", "First", "Second", "Third"],
+  running: true, // boolean
+  gear: ["Neutral", "First", "Second", "Third"], // array
   
   gear_ratio: function(speed) {
     let gearType = typeof speed;
     
-    if (gearType !== "number") {
+    if (gearType !== "number") { // Checking if the speed value is a number
       console.log("Please type in a nummerical value.")
-    } else if (speed === 0) {
+    } else if (speed === 0) { // Using the "gear" array to log out the respective value
       console.log(car.gear[0] + ": " + speed);
     } else if (speed >= 1 && speed <= 10) {
       console.log(car.gear[1] + ": " + speed);
@@ -31,4 +33,5 @@ const car = {
   }
 }
 
+// type any number between 0 and 50 in "car.gear_ratio()" to get a "gear" value
 let speedRatio = car.gear_ratio(25);
